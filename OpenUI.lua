59282c973c55331621adc72608c11544245b920c
ChinaQY <743282937@qq.com>
2025-06-09 08:02:13 +0800

    更新 OpenUI.lua

--- OpenUI.lua
+++ OpenUI.lua
@@ -15,10 +15,10 @@ UICorner.CornerRadius = UDim.new(0, 200)
 UICorner.Parent = ImageButton
 ImageButton.MouseButton1Down:connect(function()
     if ToggleUI == false then
-        game.CoreGui.ui.Main:TweenSize(UDim2.new(0, 0, 0, 0), "In", "Quad", 0.4, true)
+        game.CoreGui.VapeUI.Main:TweenSize(UDim2.new(0, 0, 0, 0), "In", "Quad", 0.4, true)
         ToggleUI = true
     else
-        game.CoreGui.ui.Main:TweenSize(UDim2.new(0, 560, 0, 320), "Out", "Quad", 0.4, true)
+        game.CoreGui.VapeUI.Main:TweenSize(UDim2.new(0, 560, 0, 320), "Out", "Quad", 0.4, true)
         ToggleUI = false
     end
 end)
