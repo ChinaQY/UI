e028e06e2bdbe30847470bbfb59c01c59533d8e7
ChinaQY <743282937@qq.com>
2025-06-17 13:32:40 +0800

    更新 OpenUI.lua

--- OpenUI.lua
+++ OpenUI.lua
@@ -1,17 +1,19 @@
 local OpenUI = Instance.new("ScreenGui")
 local ImageButton = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
-OpenUI.Name = "OpenUI"
-OpenUI.Parent = game.CoreGui
+OpenUI.Name = "Fake Block"
+OpenUI.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.AbilityContainer
 OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 ImageButton.Parent = OpenUI
-ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton.BackgroundTransparency = 0.500
-ImageButton.Position = UDim2.new(0.81, 0, 0, 0)
-ImageButton.Size = UDim2.new(0, 100, 0, 100)
-ImageButton.Image = "rbxassetid://3570695787"
-ImageButton.Draggable = true
-UICorner.CornerRadius = UDim.new(0, 200)
+ImageButton.ImageColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BackgroundTransparency = 1
+ImageButton.Position = UDim2.new(0.81, -105, 0, 275)
+ImageButton.Size = UDim2.new(0, 70, 0, 70)
+ImageButton.Image = "rbxassetid://137576632569674"
+ImageButton.BorderSizePixel = 0
+ImageButton.LayoutOrder = 1
 UICorner.Parent = ImageButton
 ImageButton.MouseButton1Down:connect(function()
     if ToggleUI == false then
